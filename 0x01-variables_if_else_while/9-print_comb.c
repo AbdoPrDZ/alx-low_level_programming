188695400da53fec5569a62caa6f5e83513ce5fc
AIabdoPr <abdopr47@gmail.com>
2023-06-19 19:43:56 +0100

    0x01-variables_if_else_while

---
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -0,0 +1,42 @@
+#include <stdio.h>
+
+/**
+* main-print the lowercase  alphabet in reverse,
+* followed by a new line
+* Return: Always 0 (Success)
+*/
+int main(void) {
+    putchar('0');
+    putchar(',');
+
+    putchar('1');
+    putchar(',');
+
+    putchar('2');
+    putchar(',');
+
+    putchar('3');
+    putchar(',');
+
+    putchar('4');
+    putchar(',');
+
+    putchar('5');
+    putchar(',');
+
+    putchar('6');
+    putchar(',');
+
+    putchar('7');
+    putchar(',');
+
+    putchar('8');
+    putchar(',');
+
+    putchar('9');
+    putchar(' ');
+
+    putchar('\n');
+
+    return 0;
+}
