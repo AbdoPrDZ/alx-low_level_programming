4767ad30b51d8b6647f8c5af608342336a6683ef
AIabdoPr <abdopr47@gmail.com>
2023-06-21 11:08:59 +0100

    0x02-functions_nested_loops

--- 0x02-functions_nested_loops/7-print_last_digit.c
+++ 0x02-functions_nested_loops/7-print_last_digit.c
@@ -2,6 +2,23 @@
 #include <stdio.h>
 
 /**
+ * _abs - Computes the absolute value of a number.
+ * @n: The number.
+ * Return: The absolute value of n.
+ */
+int _abs(int n)
+{
+	if (n < 0)
+	{
+		return (-n);
+	}
+	else
+	{
+		return (n);
+	}
+}
+
+/**
  * print_last_digit - Prints the last digit of a number.
  * @n: The number.
  * Returns: the value of the last digit
