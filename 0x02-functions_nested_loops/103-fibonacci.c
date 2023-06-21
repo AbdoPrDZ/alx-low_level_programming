e3ba12de8642250065b9865606137e7b0fa8bb47
AIabdoPr <abdopr47@gmail.com>
2023-06-21 13:27:11 +0100

    0x02-functions_nested_loops

--- 0x02-functions_nested_loops/103-fibonacci.c
+++ 0x02-functions_nested_loops/103-fibonacci.c
@@ -9,20 +9,23 @@
 int main(void)
 {
 	int i, first = 1, second = 2, next;
+	int sum = 2;
 
-	for (i = 3; i < 29; i++) {
+	for (i = 3; i < 29; i++)
+	{
 		next = first + second;
 		if (next % 2 == 0)
 		{
-			printf("%d, ", next);
+			sum += next;
 		}
 		first = second;
 		second = next;
 	}
+
 	next = first + second;
 	if (next % 2 == 0)
 	{
-		printf("%d\n", next);
+		sum += next;
 	}
 
 	return (0);
