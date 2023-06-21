606ef95aca7a8a1ea1fb9084feb0b2cf98976323
AIabdoPr <abdopr47@gmail.com>
2023-06-21 10:31:25 +0100

    0x02-functions_nested_loops

--- 0x02-functions_nested_loops/3-islower.c
+++ 0x02-functions_nested_loops/3-islower.c
@@ -7,29 +7,14 @@
  * @c: The character.
  * Return: 1 if the character is lowercase, 0 otherwise.
  */
-int _islower(int c) {
-	if(c >= 97 && c <= 122) {
+int _islower(int c)
+{
+	if(c >= 97 && c <= 122)
+	{
 		return (1);
-	} else {
+	}
+	else
+	{
 		return (0);
 	}
-}
-
-/**
- * main - check the code
- *
- * Return: Always 0.
- */
-int main(void)
-{
-	int r;
-
-	r = _islower('H');
-	putchar(r + '0');
-	r = _islower('o');
-	putchar(r + '0');
-	r = _islower(108);
-	putchar(r + '0');
-	putchar('\n');
-	return (0);
 }
