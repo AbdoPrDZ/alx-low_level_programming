ee79ac2f08e9c8a0d020c525acd7a79196dde9da
AIabdoPr <abdopr47@gmail.com>
2023-06-21 13:33:06 +0100

    0x02-functions_nested_loops

--- 0x02-functions_nested_loops/104-fibonacci.c
+++ 0x02-functions_nested_loops/104-fibonacci.c
@@ -8,5 +8,18 @@
  */
 int main(void)
 {
+	int i, first = 1, second = 2, next;
+
+	printf("%d, %d", first, second);
+
+	for (i = 3; i <= 98; i++)
+	{
+		next = first + second;
+		printf(", %d", next);
+		first = second;
+		second = next;
+	}
+
+	printf("\n");
 	return (0);
 }
