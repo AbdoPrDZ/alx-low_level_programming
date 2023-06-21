f52ae9dbb16e1c957c6305f35298c8e82ccc0ea8
AIabdoPr <abdopr47@gmail.com>
2023-06-21 12:51:41 +0100

    0x02-functions_nested_loops

--- 0x02-functions_nested_loops/102-fibonacci.c
+++ 0x02-functions_nested_loops/102-fibonacci.c
@@ -9,31 +9,32 @@
  */
 int main(void)
 {
-    mpz_t first, second, next;
-    int i;
+	mpz_t first, second, next;
+	int i;
 
-    mpz_init(first);
-    mpz_init(second);
-    mpz_init(next);
-    
-    mpz_set_ui(first, 0);
-    mpz_set_ui(second, 1);
+	mpz_init(first);
+	mpz_init(second);
+	mpz_init(next);
+	
+	mpz_set_ui(first, 0);
+	mpz_set_ui(second, 1);
 
 	gmp_printf("%d, %d, ", first, second);
 
 	for (i = 0; i < 48; i++)
 	{
-        mpz_add(next, first, second);
+		mpz_add(next, first, second);
 		gmp_printf("%d, ", next);
-        mpz_set(first, second);
-        mpz_set(second, next);
+		mpz_set(first, second);
+		mpz_set(second, next);
 	}
 
-	gmp_printf("%d\n", first + second);
+	mpz_add(next, first, second);
+	gmp_printf("%d\n", next);
 
-    mpz_clear(first);
-    mpz_clear(second);
-    mpz_clear(next);
+	mpz_clear(first);
+	mpz_clear(second);
+	mpz_clear(next);
 
 	return (0);
 }
