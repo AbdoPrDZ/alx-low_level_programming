1fe176922b524b844e2eae7d92236ec2399710cc
AIabdoPr <abdopr47@gmail.com>
2023-06-23 21:39:02 +0100

    0x04-more_functions_nested_loops

--- 0x04-more_functions_nested_loops/7-print_diagonal.c
+++ 0x04-more_functions_nested_loops/7-print_diagonal.c
@@ -12,17 +12,13 @@ void print_diagonal(int n)
 
 		for (i = 0; i < n; i++)
 		{
-			for (j = 0; j < n; j++)
-			{
-				_putchar(' ');
-			}
+			for (j = 0; j < i; j++)
+				putchar(' ');
 			
-			_putchar('\\');
-			_putchar('\n');
+			putchar('\\');
+			putchar('\n');
 		}
 	}
 	else
-	{
-		_putchar('\n');
-	}
+		putchar('\n');
 }
--- 0x04-more_functions_nested_loops/main.c
+++ 0x04-more_functions_nested_loops/main.c
@@ -50,5 +50,13 @@ int main(void)
     print_line(-4);
     */
 
+   	/* 7-print_diagonal.c */
+   	/* */
+    print_diagonal(0);
+    print_diagonal(2);
+    print_diagonal(10);
+    print_diagonal(-4);
+	
+
 	return (0);
 }
