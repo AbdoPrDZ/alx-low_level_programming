0188123a250ba54d9d954971cff01b608dcd5c90
AIabdoPr <abdopr47@gmail.com>
2023-06-23 22:57:42 +0100

    0x04-more_functions_nested_loops

--- 0x04-more_functions_nested_loops/main.c
+++ 0x04-more_functions_nested_loops/main.c
@@ -9,29 +9,29 @@ int main(void)
 {
 	/* 0-isupper */
 	/*
-    char c;
+	char c;
 
 	c = 'A';
 	printf("%c: %d\n", c, _isupper(c));
 	c = 'a';
 	printf("%c: %d\n", c, _isupper(c));
-    */
+	*/
 
 	/* 1-isdigit */
-    /*
+	/*
 	char c;
 
 	c = '0';
 	printf("%c: %d\n", c, _isdigit(c));
 	c = 'a';
 	printf("%c: %d\n", c, _isdigit(c));
-    */
+	*/
 
 	/* 2-mul */
-    /*
+	/*
 	printf("%d\n", mul(98, 1024));
 	printf("%d\n", mul(-402, 4096));
-    */
+	*/
 
 	/* 3-print_numbers */
 	/* print_numbers();*/
@@ -42,36 +42,36 @@ int main(void)
 	/* 5-more_numbers */
 	/* more_numbers(); */
 
-    /* 6-print_line */
-    /*
-    print_line(0);
-    print_line(2);
-    print_line(10);
-    print_line(-4);
-    */
+	/* 6-print_line */
+	/*
+	print_line(0);
+	print_line(2);
+	print_line(10);
+	print_line(-4);
+	*/
 
    	/* 7-print_diagonal.c */
-   	/* 
-    print_diagonal(0);
-    print_diagonal(2);
-    print_diagonal(10);
-    print_diagonal(-4);
+   	/*
 	*/
+	print_diagonal(0);
+	print_diagonal(2);
+	print_diagonal(10);
+	print_diagonal(-4);
 
 	/* 8-print_square.c */
 	/*
-    print_square(2);
-    print_square(10);
-    print_square(0);
+	print_square(2);
+	print_square(10);
+	print_square(0);
 	*/
 
 	/* 10-print_triangle.c */
 	/*
+	print_triangle(2);
+	print_triangle(10);
+	print_triangle(1);
+	print_triangle(0);
 	*/
-    print_triangle(2);
-    print_triangle(10);
-    print_triangle(1);
-    print_triangle(0);
 
 	return (0);
 }
