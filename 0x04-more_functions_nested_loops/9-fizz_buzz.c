6238027e7cf03cc38db4fba992d77b835a204287
AIabdoPr <abdopr47@gmail.com>
2023-06-23 22:12:58 +0100

    0x04-more_functions_nested_loops

--- 0x04-more_functions_nested_loops/9-fizz_buzz.c
+++ 0x04-more_functions_nested_loops/9-fizz_buzz.c
@@ -11,21 +11,13 @@ int main(void)
 	for (i = 1; i <= 100; i++)
 	{
 		if (i % 3 == 0)
-		{
-			printf("Fizz\n");
-		}
+			printf("Fizz ");
 		else if (i % 5 == 0)
-		{
-			printf("Buzz\n");
-		}
+			printf("Buzz ");
 		else if ((i % 3 == 0) && (i % 5 == 0))
-		{
-			printf("FizzBuzz\n");
-		}
+			printf("FizzBuzz ");
 		else
-		{
-		   printf("%d\n", i);
-		}
+		   printf("%d ", i);
 	}
 
 	return (0);
