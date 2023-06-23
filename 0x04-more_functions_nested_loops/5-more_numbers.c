140e64920f871a7894012d18cee1919e260381b9
AIabdoPr <abdopr47@gmail.com>
2023-06-23 21:30:30 +0100

    0x04-more_functions_nested_loops

--- 0x04-more_functions_nested_loops/5-more_numbers.c
+++ 0x04-more_functions_nested_loops/5-more_numbers.c
@@ -3,7 +3,7 @@
 /**
  * more_numbers - Prints 10 times the numbers, from 0 to 14.
  */
-int main(void)
+void more_numbers(void)
 {
 	int i, j;
 
@@ -11,12 +11,10 @@ int main(void)
 	{
 		for (j = 0; j < 15; j++)
 		{
-			if (j >= 10)  _putchar('1');
+			if (j >= 10)  putchar('1');
 
-			_putchar('0' + j % 10);
+			putchar('0' + j % 10);
 		}
-		_putchar('\n');
+		putchar('\n');
 	}
-
-	return (0);
 }
--- 0x04-more_functions_nested_loops/main.c
+++ 0x04-more_functions_nested_loops/main.c
@@ -7,6 +7,40 @@
  */
 int main(void)
 {
-    print_most_numbers();
-    return (0);
+	/* 0-isupper */
+	/*
+    char c;
+
+	c = 'A';
+	printf("%c: %d\n", c, _isupper(c));
+	c = 'a';
+	printf("%c: %d\n", c, _isupper(c));
+    */
+
+	/* 1-isdigit */
+    /*
+	char c;
+
+	c = '0';
+	printf("%c: %d\n", c, _isdigit(c));
+	c = 'a';
+	printf("%c: %d\n", c, _isdigit(c));
+    */
+
+	/* 2-mul */
+    /*
+	printf("%d\n", mul(98, 1024));
+	printf("%d\n", mul(-402, 4096));
+    */
+
+	/* 3-print_numbers */
+	/* print_numbers();*/
+
+	/* 4-print_most_numbers */
+	/* print_most_numbers(); */
+
+	/* 5-more_numbers */
+	more_numbers();
+
+	return (0);
 }
