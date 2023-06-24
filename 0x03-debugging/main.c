d20a53da68ffef6bcfcead601451b8d9c9b194b0
AIabdoPr <abdopr47@gmail.com>
2023-06-24 09:44:43 +0100

    0x03-debugging

--- 0x03-debugging/main.c
+++ 0x03-debugging/main.c
@@ -7,6 +7,8 @@
 
 int main(void)
 {
+	/* 2-largest_number.c */
+	/*
 	int a, b, c;
 	int largest;
 
@@ -17,6 +19,22 @@ int main(void)
 	largest = largest_number(a, b, c);
 
 	printf("%d is the largest number\n", largest);
+	*/
+
+	/* 3-print_remaining_days.c */
+	int month;
+	int day;
+	int year;
+
+	month = 4;
+	day = 01;
+	year = 1997;
+
+	printf("Date: %02d/%02d/%04d\n", month, day, year);
+
+	day = convert_day(month, day);
+
+	print_remaining_days(month, day, year);
 
 	return (0);
 }
