ddac79f91507bda080017fafbb501ad0bff2d685
AIabdoPr <abdopr47@gmail.com>
2023-06-27 09:58:24 +0100

    0x05-pointers_arrays_strings

--- 0x05-pointers_arrays_strings/100-atoi.c
+++ 0x05-pointers_arrays_strings/100-atoi.c
@@ -2,53 +2,35 @@
 #include <string.h>
 
 /**
- * _pow - calculate the power
- * @n: The number.
- * @p: The power.
- * Return: The power of n.
- */
-int _pow(int n, int p)
-{
-	int i, r;
-
-	r = 1;
-
-	for (i = 0; i < p; i++)
-	{
-		r *= n;
-	}
-
-	return (r);
-}
-
-/**
  * _atoi - convert string to int.
- * 
  * @str: The string.
  * Return: The int.
  */
 int _atoi(char *str)
 {
-	int i, num, start;
+	int i, num, start, sign;
 
 	num = 0;
 	start = -1;
+	sign = 1;
 
 	for (i = 0; str[i] != '\0'; i++)
 	{
+		if (str[i] == '-')
+			sign = -1;
+
 		if (str[i] >= '0' && str[i] <= '9')
 		{
 			if (start == -1)
 				start = i;
 
-            num = num * 10 + (str[i] - '0');
+			num = num * 10 + (str[i] - '0');
 		}
 		else if (start != -1)
 			break;
 	}
 
-	if (start > 0 && str[start - 1] == '-')
-		num = -num;
+	num *= sign;
 
 	return (num);
 }
--- 0x05-pointers_arrays_strings/main.c
+++ 0x05-pointers_arrays_strings/main.c
@@ -122,6 +122,10 @@ int main(void)
 	printf("%d\n", nb);
 	nb = _atoi("---++++ -++ Sui - te -   402 #cisfun :)");
 	printf("%d\n", nb);
+	nb = _atoi("+++++    +-+    2242454");
+	printf("%d\n", nb);
+	nb = _atoi("-2147483648");
+	printf("%d\n", nb);
 	*/
 
 	return (0);
