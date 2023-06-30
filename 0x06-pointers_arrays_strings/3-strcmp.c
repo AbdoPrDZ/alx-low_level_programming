f00c66c6493634c2427474ef0a78be34a5006f24
AIabdoPr <abdopr47@gmail.com>
2023-06-30 22:26:52 +0100

    0x06-pointers_arrays_strings

---
+++ 0x06-pointers_arrays_strings/3-strcmp.c
@@ -0,0 +1,22 @@
+#include "main.h"
+
+/**
+ * _strcmp - Compares two strings.
+ * @s1: The first string.
+ * @s2: The second string.
+ * Return: 0 if the strings are equal, negative if s1 < s2, positive if s1 > s2.
+ */
+int _strcmp(char *s1, char *s2)
+{
+    int i = 0;
+
+    while (s1[i] == s2[i])
+    {
+        if (s1[i] == '\0')
+            return 0;
+
+        i++;
+    }
+
+    return s1[i] - s2[i];
+}
---
+++ 0x06-pointers_arrays_strings/6-cap_string.c
@@ -0,0 +1,32 @@
+#include "main.h"
+#include <ctype.h>
+
+/**
+ * cap_string - Capitalizes all words in a string.
+ * @str: The input string.
+ * Return: A pointer to the modified string.
+ */
+char *cap_string(char *str)
+{
+    int i;
+    int capitalize_next = 1;
+
+    for (i = 0; str[i] != '\0'; i++)
+    {
+        if (str[i] == ' ' || str[i] == '\t' || str[i] == '\n' ||
+            str[i] == ',' || str[i] == ';' || str[i] == '.' ||
+            str[i] == '!' || str[i] == '?' || str[i] == '"' ||
+            str[i] == '(' || str[i] == ')' || str[i] == '{' ||
+            str[i] == '}')
+        {
+            capitalize_next = 1;
+        }
+        else if (capitalize_next && isalpha(str[i]))
+        {
+            str[i] = toupper(str[i]);
+            capitalize_next = 0;
+        }
+    }
+
+    return str;
+}
