f7d42f20dadebfeef4d43649f8d6570764b50b1c
AIabdoPr <abdopr47@gmail.com>
2023-07-03 21:52:08 +0100

    0x07-pointers_arrays_strings

--- 0x07-pointers_arrays_strings/main.c
+++ 0x07-pointers_arrays_strings/main.c
@@ -133,5 +133,15 @@ int main(void)
 	print_diagsums((int *)c5, 5);
 	*/
 
+	/* 100-main.c */
+	/*
+	char *s0 = "Bob Dylan";
+	char *s1 = "Robert Allen";
+
+	printf("%s, %s\n", s0, s1);
+	set_string(&s1, s0);
+	printf("%s, %s\n", s0, s1);
+	*/
+
 	return (0);
 }
