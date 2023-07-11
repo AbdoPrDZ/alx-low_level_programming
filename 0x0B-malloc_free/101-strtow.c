af8ac2cd562d233cbf9e5763fdfe3d8468283b10
AIabdoPr <abdopr47@gmail.com>
2023-07-11 19:54:10 +0100

    0x0B-malloc_free

--- 0x0B-malloc_free/101-strtow.c
+++ 0x0B-malloc_free/101-strtow.c
@@ -24,6 +24,26 @@ char *getWord(char *s, int start, int end)
 	return (w);
 }
 
+int count_words(char *str)
+{
+	int i, s = -1, e = -1, wc = 0;
+
+	for (i = 0; i < (int) strlen(str); i++)
+	{
+		if (str[i] != ' ' && s < 0)
+			s = i;
+		else if ((str[i] == ' ' || str[i] == '\t' || str[i] == '\n') && s >= 0)
+			e = i;
+		else if (str[i + 1] == '\0' && s >= 0)
+			e = i + 1;
+
+		if (s >= 0 && e > 0)
+			wc++;
+	}
+
+	return (wc);
+}
+
 /**
  * strtow - splits a string into words.
  * @str: the string.
@@ -31,14 +51,20 @@ char *getWord(char *s, int start, int end)
  */
 char **strtow(char *str)
 {
-	int i, wCount = 0, s = -1, e = -1;
+	int i, wCount = 0, wc, s = -1, e = -1;
 
 	char **words;
 
 	if (str == NULL || str[0] == '\0')
 		return (NULL);
 
-	words = malloc(4 * sizeof(int *));
+	/*words = malloc(1 * sizeof(char *));*/
+	wc = count_words(str);
+
+	if (wc == 0)
+		return (NULL);
+
+	words = malloc((wc + 1) * sizeof(char *));
 
 	if (str[0] != ' ')
 		s = 0;
@@ -56,7 +82,7 @@ char **strtow(char *str)
 		{
 			char *word = getWord(str, s, e);
 
-			words = realloc(words, (wCount + 1) * sizeof(char *));
+			/*words = realloc(words, (wCount + 1) * sizeof(char *));*/
 			words[wCount] = word;
 			wCount++;
 			s = -1;
@@ -64,10 +90,12 @@ char **strtow(char *str)
 		}
 	}
 
+	/*
 	if (wCount == 0)
 		return (NULL);
 
 	words = realloc(words, (wCount + 1) * sizeof(char *));
+	*/
 	words[wCount] = NULL;
 
 	return (words);
