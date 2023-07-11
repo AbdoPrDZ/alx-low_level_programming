0c3a619b2a10b6e5326a98c3950f375b6c61f89b
AIabdoPr <abdopr47@gmail.com>
2023-07-11 17:24:04 +0100

    0x0B-malloc_free

--- 0x0B-malloc_free/1-strdup.c
+++ 0x0B-malloc_free/1-strdup.c
@@ -11,10 +11,13 @@ char *_strdup(char *str)
 	char *s;
 	long int i;
 
-	if (strlen(str) == 0)
+	if (str == NULL)
 		return (NULL);
 
 	s = malloc(sizeof(char) * (strlen(str)));
+
+	if (s == NULL)
+		return (NULL);
 
 	for (i = 0; i < (long int) strlen(str); i++)
 		s[i] = str[i];
