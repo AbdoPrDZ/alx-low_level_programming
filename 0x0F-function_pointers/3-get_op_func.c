c8e51e59f53e66c185bc00174e0b883d6ac6a94e
AIabdoPr <abdopr47@gmail.com>
2023-07-20 21:01:32 +0100

    0x0F-function_pointers

--- 0x0F-function_pointers/3-get_op_func.c
+++ 0x0F-function_pointers/3-get_op_func.c
@@ -1,26 +1,6 @@
 #include "3-calc.h"
 
 /**
- * search_for_op - search for operation index
- *
- * @s: operation name.
- * @ops: operations array.
- * @index: current index.
- * Return: index of operation or -1 if not found.
- */
-int search_for_op(char *s, op_t ops[], int index)
-{
-	if (strcmp(ops[index].op, s) == 0)
-		return (index);
-
-	index++;
-	if (ops[index].op != NULL)
-		return (search_for_op(s, ops, index));
-
-	return (-1);
-}
-
-/**
  * get_op_func - getting operation pointer function.
  * @s: operation name.
  * Return: the pointer to the function, or NULL when not found.
@@ -35,12 +15,16 @@ int (*get_op_func(char *s))(int, int)
 		{"%", op_mod},
 		{NULL, NULL}
 	};
+
 	int i;
 
-	i = search_for_op(s, ops, 0);
+	while (ops[i].op != NULL)
+	{
+		if (strcmp(ops[i].op, s) == 0)
+			return (ops[i].f);
 
-	if (i != -1)
-		return (ops[i].f);
+		i++;
+	}
 
 	return (NULL);
 }
