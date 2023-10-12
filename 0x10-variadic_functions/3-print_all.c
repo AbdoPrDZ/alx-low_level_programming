e9371cabc396a0b90bd84b593f9733ef9ca7e12c
AbdoPrD2 <abdopr47@gmail.com>
2023-10-12 10:19:47 +0100

    0x10-variadic_functions

---
+++ 0x10-variadic_functions/3-print_all.c
@@ -0,0 +1,73 @@
+#include "variadic_functions.h"
+
+/**
+ * printChar - print char arg.
+ * @args: arguments list.
+ */
+void printChar(va_list args)
+{
+	printf("%c", va_arg(args, int));
+}
+
+/**
+ * printInt - print int arg.
+ * @args: arguments list.
+ */
+void printInt(va_list args)
+{
+	printf("%d", va_arg(args, int));
+}
+
+/**
+ * printFloat - print float arg.
+ * @args: arguments list.
+ */
+void printFloat(va_list args)
+{
+	printf("%f", va_arg(args, double));
+}
+
+/**
+ * printString - print string arg.
+ * @args: arguments list.
+ */
+void printString(va_list args)
+{
+	char *s = va_arg(args, char *);
+
+	printf("%s", (s != NULL) ? s : "(nil)");
+}
+
+/**
+ * print_all - prints anything.
+ * @format: format of arguments.
+ */
+void print_all(const char *const format, ...)
+{
+	tp_t tps[] = {
+		{"c", printChar},
+		{"i", printInt},
+		{"f", printFloat},
+		{"s", printString},
+		{NULL, NULL}};
+
+	int i = 0, j = 0;
+
+	va_list args;
+
+	va_start(args, format);
+
+	for (j = 0; format[i] != '\0'; i++)
+		for (; tps[j].tpc != NULL; j++)
+			if (tps[i].tpc[0] == format[j])
+			{
+				tps[i].printer(args);
+				if (format[i + 1] != '\0')
+					printf(", ");
+				break;
+			}
+
+	va_end(args);
+
+	printf("\n");
+}
---
+++ 0x10-variadic_functions/main.c
@@ -0,0 +1,36 @@
+#include "variadic_functions.h"
+
+/**
+ * main - check the code
+ *
+ * Return: Always 0.
+ */
+int main(void)
+{
+    /* 0-main.c */
+    /*
+    int sum;
+
+    sum = sum_them_all(2, 98, 1024);
+    printf("%d\n", sum);
+    sum = sum_them_all(4, 98, 1024, 402, -1024);
+    printf("%d\n", sum);
+    */
+
+    /* 1-main.c */
+    /*
+    print_numbers(", ", 4, 0, 98, -1024, 402);
+    */
+
+    /* 2-main.c */
+    /*
+    print_strings(", ", 2, "Jay", "Django");
+    */
+
+    /* 3-main.c */
+    /*
+    print_all("ceis", 'B', 3, "stSchool");
+    */
+
+    return (0);
+}
