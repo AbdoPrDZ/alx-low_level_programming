ccf84862c97018efd543aac734e782fd2b13a32d
AbdoPrDZ <abdopr47@gmail.com>
2023-12-11 19:36:48 +0100

    0x18-dynamic_libraries

---
+++ 0x18-dynamic_libraries/100-operations.c
@@ -0,0 +1,56 @@
+#include "main.h"
+
+/**
+ * add - calculate add of a and b
+ * @a: the first number
+ * @b: the second number
+ * Return: the add of a and b
+ */
+int add(int a, int b)
+{
+	return (a + b);
+}
+
+/**
+ * sub - calculate sub of a and b
+ * @a: the first number
+ * @b: the second number
+ * Return: the sub of a and b
+ */
+int sub(int a, int b)
+{
+	return (a - b);
+}
+
+/**
+ * mul - calculate mul of a and b
+ * @a: the first number
+ * @b: the second number
+ * Return: the mul of a and b
+ */
+int mul(int a, int b)
+{
+	return (a * b);
+}
+
+/**
+ * div - calculate div of a and b
+ * @a: the first number
+ * @b: the second number
+ * Return: the div of a and b
+ */
+int div(int a, int b)
+{
+	return (a / b);
+}
+
+/**
+ * mod - calculate mod of a and b
+ * @a: the first number
+ * @b: the second number
+ * Return: the mod of a and b
+ */
+int mod(int a, int b)
+{
+	return (a % b);
+}
